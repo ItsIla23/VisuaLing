fb91597ff8dbbedaa5e77b668dce15789f4bfd58
Ilaria Poziello <148434755+ItsIla23@users.noreply.github.com>
2024-06-24 17:19:48 +0200

    Add files via upload

---
+++ VisuaLing/VisuaLingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisuaLingApp.swift
+//  VisuaLing
+//
+//  Created by Ilaria Poziello on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct VisuaLingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CameraView()
+        }
+    }
+}
